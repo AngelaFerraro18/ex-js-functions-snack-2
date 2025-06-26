3fe47c4c994d528331aace4f274a6327db407de4
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-06-26 11:48:15 +0200

    feat: snack 2 completed

--- app.js
+++ app.js
@@ -18,3 +18,13 @@
 let somma = (num1, num2) => num1 + num2;
 
 console.log(somma(55, 9));
+
+
+/* Crea una arrow function che calcola il quadrato di un numero. 
+Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+*/
+
+const quadrato = (num) => num * num;
+
+console.log(quadrato(2));
+
