5e5ad3e952add18d4446af60561ffe9d78285ea4
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-06-26 11:45:52 +0200

    feat: snack 1 completed

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('ciao')
+/* Crea una funzione che somma due numeri: 
+- crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma;
+- Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile;
+-Quindi, riscrivi la funzione somma con la sintassi delle arrow functions.
+*/
+
+
+// function somma(num1, num2) {
+//     return num1 + num2;
+// }
+
+// console.log(somma(3, 8));
+
+// let somma = function (num1, num2) { return num1 + num2 }
+
+// console.log(somma(40, 5));
+
+let somma = (num1, num2) => num1 + num2;
+
+console.log(somma(55, 9));
